f2a65a8aa2f344a8822d382415c817beefb6967e
Natasha Waller <natashawaller@Natashas-MacBook-Pro.local>
2019-02-02 12:18:05 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1 , :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+   @length_1= length_1
+   @length_2 = length_2
+   @length_3 = length_3
+  end
+
+  def kind
+    #if triangle is illegal raise a custom error that the triangle is invalid --- the sum of the length
+      #of any two sides is not more than the length of the 3rd side or each side is 
+      #not longer than 0
+    #elsif condition met :equilateral  same length on all sides 
+    #elsif condition met :isosceles two sides same legth
+    #elsif condition met :scalene  all sides different lengths 
+   # Not sure why I didn't need to have the begin and rescue wording below, but it all functions properly
+     # begin
+      # raise TriangleError
+      #rescue TriangleError => error
+      #  puts error.message
+     # end
+    
+    if !((length_1 + length_2) > length_3) || !((length_1 + length_3) > length_2) || !((length_2 + length_3) > length_1)
+         raise TriangleError
+         puts error.message
+    
+    elsif length_1 == length_2 && length_2 == length_3
+      :equilateral
+  
+    elsif length_1 == length_3 && length_2 != length_1
+      :isosceles
+    elsif length_2 == length_3 && length_2 != length_1
+      :isosceles
+    elsif length_1 == length_2 && length_2 != length_3
+      :isosceles
+    elsif length_1 != length_2 && length_1 != length_3
+      :scalene
+    end
+  end
+    
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle! A triangle must have sides that are longer than 0 and the sum of the length of any two sides must be more than the length of the 3rd side."
+  
+    end
+  end
+
 end
